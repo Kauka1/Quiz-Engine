eac26e0375ce03bdce8d0422bca691147f3c996c
Kay <kauuka@gmail.com>
2021-09-18 22:59:25 -0700

    security work started

--- Web Quiz Engine/back-end/src/engine/business/security/MyUserDetails.java
+++ Web Quiz Engine/back-end/src/engine/business/security/MyUserDetails.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+//UserDetails is disabled while security is not implemented
 public class MyUserDetails implements UserDetails {
     private String userName;
     private String password;
